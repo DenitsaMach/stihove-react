8028e9bb22a39c6c3bccb12982718004814c7fbb
DenitsaMach <d.machkarska@gmail.com>
2021-12-12 00:51:22 +0200

    Add Forms for Login and Register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,83 @@
-function Login () {
+import { useState } from 'react';
+
+import * as yup from 'yup';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
+function Login() {
+    const zeroErrors = {email: '', password: ''};
+    const allTouched = (isIt) => ({email: isIt, password: isIt});
+    const [error, setError] = useState(zeroErrors);
+    const [isTouched, setIsTouched] = useState(allTouched(false));
+    const validationRules = {
+        email: yup.string().email().required(),
+        password: yup.string().required()
+    };
+    const onChangeHandler = (event) => {
+        event.preventDefault();
+        let newError = {...error};
+        if (!isTouched[event.target.name]) {
+            let newIsTouched = {...isTouched};
+            newIsTouched[event.target.name] = true;
+            setIsTouched(newIsTouched);
+        }
+        validationRules[event.target.name].validate(event.target.value)
+            .then(function () {
+                newError[event.target.name] = '';
+                setError(newError);
+            }).catch(function (err) {
+                newError[event.target.name] = err.message;
+                setError(newError);
+            });
+    }
+    const schema = yup.object().shape(validationRules);
+    const onLoginHandler = (event) => {
+        event.preventDefault();
+        let formData = new FormData(event.currentTarget);
+        let formDataObj = Object.fromEntries(formData.entries());
+        schema.validate(formDataObj, { abortEarly: false })
+            .then(function (validData) {
+                console.log(validData);
+                //todo: send to server
+            }).catch(function (err) {
+                let newError = zeroErrors;
+                err.inner.forEach(e => {
+                    newError[e.path] = e.message;
+                });
+                setError(newError);
+                setIsTouched(allTouched(true));
+            });
+
+        let email = formData.get('email');
+        let password = formData.get('password');
+
+        /* authService.login(email, password)
+            .then((authData) => {
+                login(authData);
+                addNotification('You logged in successfully', types.success);
+                navigate('/dashboard');
+            })
+            .catch(err => {
+                // TODO: show notification
+                console.log(err);
+            });
+        } */
+    }
+
     return (
-        <div></div>
+        <Form noValidate onSubmit={onLoginHandler} onChange={onChangeHandler} >
+            <Form.Label as="legend" column sm={2}>Вход:</Form.Label>
+            <FloatingLabel controlId="registerEmail" label="Имейл" className="mb-3">
+                <Form.Control name="email" type="email" isValid={isTouched.email && !error.email} isInvalid={!!error.email} />
+                <Form.Control.Feedback type="invalid">{error.email}</Form.Control.Feedback>
+            </FloatingLabel>
+            <FloatingLabel controlId="registerPassword" label="Парола" className="mb-3">
+                <Form.Control name="password" type="password" isValid={isTouched.password && !error.password} isInvalid={!!error.password} />
+                <Form.Control.Feedback type="invalid">{error.password}</Form.Control.Feedback>
+            </FloatingLabel>
+            <Button type="submit">Влез!</Button>
+        </Form>
     );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,79 @@
-function Register () {
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+
+function Register() {
+    const initialValues = {
+        email: '',
+        name: '',
+        surname: '',
+        password: '',
+        passwordRepeat: '',
+    };
+    const validationRules = {
+        email: yup.string().email().required(),
+        name: yup.string().required(),
+        surname: yup.string().required(),
+        password: yup.string().required(),
+        passwordRepeat: yup.mixed().test('pass-test', 'Must be the same as password', function (value) {
+            let { password } = this.parent;
+            return value === password
+         }),
+    };
+    const onSubmitHandler = async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+    }
+    const schema = yup.object().shape(validationRules);
     return (
-        <div></div>
+        <Formik validationSchema={schema} onSubmit={onSubmitHandler} initialValues={{}}>
+            {({ handleSubmit, handleChange, handleBlur, touched, isValid, errors, }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                    <Form.Label as="legend" column sm={2}>Регистрация:</Form.Label>
+                    <Row>
+                        <Col>
+                            <FloatingLabel controlId="email" label="Имейл" className="mb-3">
+                                <Form.Control type="email" name="email" isValid={touched.email && !errors.email} isInvalid={touched.email && errors.email} onChange={handleChange} onBlur={handleBlur} />
+                                <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <FloatingLabel controlId="name" label="Име" className="mb-3">
+                                <Form.Control type="text" name="name" isValid={touched.name && !errors.name && touched.name} isInvalid={touched.name && errors.name} onChange={handleChange} onBlur={handleBlur} />
+                                <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                        <Col>
+                            <FloatingLabel controlId="surname" label="Фамилия" className="mb-3">
+                                <Form.Control type="text" name="surname" isValid={touched.surname && !errors.surname} isInvalid={touched.surname && errors.surname} onChange={handleChange} onBlur={handleBlur} />
+                                <Form.Control.Feedback type="invalid">{errors.surname}</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <FloatingLabel controlId="password" label="Парола" className="mb-3">
+                                <Form.Control type="password" name="password" isValid={touched.password && !errors.password} isInvalid={touched.password && errors.password} onChange={handleChange} onBlur={handleBlur} />
+                                <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                        <Col>
+                            <FloatingLabel controlId="passwordRepeat" label="Повтори паролата" className="mb-3">
+                                <Form.Control type="password" name="passwordRepeat" isValid={touched.passwordRepeat && !errors.passwordRepeat} isInvalid={!!errors.passwordRepeat} onChange={handleChange} onBlur={handleBlur} />
+                                <Form.Control.Feedback type="invalid">{errors.passwordRepeat}</Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Col>
+                    </Row>
+                    <Button type="submit" disabled={!isValid}>Регистрирай ме!</Button>
+                </Form>
+            )}
+        </Formik>
     );
 }
 
